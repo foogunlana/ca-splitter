f1c3d3811300f88c714419d195187db7023e7051
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-09 22:43:43 +0100

    fixed declaration bug

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -25,7 +25,7 @@ contract Splitter is OwnedDestroyable{
     function Splitter(address _sender) {
         sender = _sender;
     }
-    
+
     function sendSplit(address[2] _recipients)
         public
         payable
@@ -34,7 +34,7 @@ contract Splitter is OwnedDestroyable{
 
         require(msg.value > 0);
         uint amountPerRecipient = msg.value / 2;
-        uint remainder = msg.value % 2;
+        remainder = msg.value % 2;
         // figure straight up indexes are better than for loops for a small fixed length array
         balances[_recipients[0]] = amountPerRecipient;
         balances[_recipients[1]] = amountPerRecipient;
