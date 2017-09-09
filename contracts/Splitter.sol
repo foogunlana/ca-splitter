064795d9a067748f39f14552f926482e0a32fbd6
Foluso Ogunlana <folusoogunlana@folusos-air.home>
2017-09-09 22:39:29 +0100

    account for remainder after sending split

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -8,6 +8,7 @@ import { OwnedDestroyable } from './OwnedDestroyable.sol';
 */
 contract Splitter is OwnedDestroyable{
     address public sender;
+    uint public remainder;
     mapping (address => uint) public balances;
 
     event LogSetSender(address indexed _sender);
@@ -24,21 +25,7 @@ contract Splitter is OwnedDestroyable{
     function Splitter(address _sender) {
         sender = _sender;
     }
-
-    function setSender(address _sender)
-        public
-        onlyOwner
-        returns(bool) {
-
-        sender = _sender;
-        LogSetSender(_sender);
-        return true;
-    }
-
-    /*
-      Should receive an ETH value and split it equally amongst all
-      recipients
-    */
+    
     function sendSplit(address[2] _recipients)
         public
         payable
@@ -47,6 +34,7 @@ contract Splitter is OwnedDestroyable{
 
         require(msg.value > 0);
         uint amountPerRecipient = msg.value / 2;
+        uint remainder = msg.value % 2;
         // figure straight up indexes are better than for loops for a small fixed length array
         balances[_recipients[0]] = amountPerRecipient;
         balances[_recipients[1]] = amountPerRecipient;
